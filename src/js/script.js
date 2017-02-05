e4c69455ef27e94bb6e193cdbf5b358356bb47e7
Mikael Sundström <admin@onlinecores.com>
2017-02-05 23:34:30 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -69,65 +69,19 @@ parseURL(currentUrl); // outputs listed below:
 //
 //
 //  http://subdomain.example.com/path/to/directory
-var theProtocol = parsed_url.protocol; //         http
-var theSubdomain = parsed_url.subdomain; //       subdomain
+//var theProtocol = parsed_url.protocol; //         http
+//var theSubdomain = parsed_url.subdomain; //       subdomain
 var theHost = parsed_url.host; //                 example
 var theTld = parsed_url.tld; //                   com
-var thePath = parsed_url.path; //                 path/to/directory
-var theDomain = parsed_url.domain; //             subdomain.example.com
+//var thePath = parsed_url.path; //                 path/to/directory
+//var theDomain = parsed_url.domain; //             subdomain.example.com
 var theFirstChar = parsed_url.host.charAt(0); //  e
 
-var pathToLocalCssFile = chrome.extension.getURL("stylesheet.css"); // "chrome-extension://pluginID/stylesheet.css"
-var pathToExternalCssFile = "https://raw.githubusercontent.com/OnlineCores/Epoxy/master/src/css/" + theTld + slash + theFirstChar + slash + theHost + dot + css;
-var pathToDomainListFile = "https://raw.githubusercontent.com/OnlineCores/Epoxy/master/src/css/" + theTld + slash + theFirstChar + slash + "list.txt";
+//var pathToLocalCssFile = chrome.extension.getURL("stylesheet.css"); // "chrome-extension://pluginID/stylesheet.css"
+var pathToExternalCssFile = "https://raw.githubusercontent.com/OnlineCores/Epoxy/master/tld/" + theTld + slash + theHost + dot + css;
+//var pathToDomainListFile = "https://raw.githubusercontent.com/OnlineCores/Epoxy/master/src/css/" + theTld + slash + theFirstChar + slash + "list.txt";
 
 var fileExists = LinkCheck(pathToExternalCssFile);
-
-
-/*
-//   ___     _        _                _        _ _    _
-//  / __|___| |_   __| |___ _ __  __ _(_)_ _   | (_)__| |_
-// | (_ / -_)  _| / _` / _ \ '  \/ _` | | ' \  | | (_-<  _|
-//  \___\___|\__| \__,_\___/_|_|_\__,_|_|_||_| |_|_/__/\__|
-//
-//
-readTextFile(pathToDomainListFile);
-
-
-function readTextFile(file) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", file, false);
-  rawFile.onreadystatechange = function () {
-    if (rawFile.readyState === 4) {
-      if (rawFile.status === 200 || rawFile.status == 0) {
-        var allText = rawFile.responseText;
-        intoArray(allText);
-      }
-    }
-  }
-  rawFile.send(null);
-}
-
-function intoArray(lines) {
-  var lineArr = lines.split('\n');
-  if (lineArr.length > 0) {
-    findDomain(lineArr);
-  } else {
-
-  }
-}
-
-function checkDomain(domain) {
-  return domain == theHost;
-}
-
-function findDomain(listedDomains) {
-  var sss = listedDomains.find(checkDomain);
-  alert(sss);
-}
-
-
-*/
 
 
 
@@ -172,21 +126,45 @@ function addElementToHead(filepath, filetype) {
     document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
-
 /*
-//  ___             _                   _  __ _       ___ ___ ___
-// | _ \___ __ _ __| |  ____ __  ___ __(_)/ _(_)__   / __/ __/ __|
-// |   / -_) _` / _` | (_-< '_ \/ -_) _| |  _| / _| | (__\__ \__ \
-// |_|_\___\__,_\__,_| /__/ .__/\___\__|_|_| |_\__|  \___|___/___/
-//                        |_|
+//   ___     _        _                _        _ _    _
+//  / __|___| |_   __| |___ _ __  __ _(_)_ _   | (_)__| |_
+// | (_ / -_)  _| / _` / _ \ '  \/ _` | | ' \  | | (_-<  _|
+//  \___\___|\__| \__,_\___/_|_|_\__,_|_|_||_| |_|_/__/\__|
 //
-// if current top domain is in the list, then run css document that match that domain
-if (domainID == "google.se") {
-  addElementToHead(pathToLocalCssFile, css)
-  //alert("if");
+//
+readTextFile(pathToDomainListFile);
 
-} else {
-  addElementToHead(pathToGeneralCssFile, css)
-  //alert("else");
 
-}*/
+function readTextFile(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
+        intoArray(allText);
+      }
+    }
+  }
+  rawFile.send(null);
+}
+
+function intoArray(lines) {
+  var lineArr = lines.split('\n');
+  if (lineArr.length > 0) {
+    findDomain(lineArr);
+  } else {
+
+  }
+}
+
+function checkDomain(domain) {
+  return domain == theHost;
+}
+
+function findDomain(listedDomains) {
+  var sss = listedDomains.find(checkDomain);
+  alert(sss);
+}
+*/
